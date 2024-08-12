f5774a0f53cf5137b7e12e33ed7c0490b522a00e
Osama Bin Azam <osamabinazam@gmail.com>
2024-08-12 19:31:15 +0500

    First commit

---
+++ src/test/java/com/dropmate/dropmatebackend/DropMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dropmate.dropmatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropMateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
